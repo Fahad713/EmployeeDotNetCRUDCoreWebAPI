fb6c4edc5152991886f4b7a54007be808e4d868c
Fahad Sheikh <99194379+Fahad713@users.noreply.github.com>
2024-12-22 17:15:09 -0800

    Adding repository to github

---
+++ AdminPortal/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using AdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext DbContext;
+
+        public EmployeeController(ApplicationDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees() {
+            var allEmployees = DbContext.Employees.ToList();
+            return Ok(allEmployees);
+
+        }
+    }
+}
+ 
